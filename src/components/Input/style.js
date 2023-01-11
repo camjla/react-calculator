a274f500d286646875f05970a7be29d75bcfab28
camjla <camila.tronco@hotmail.com>
2023-01-11 15:55:06 -0300

    calculator-react

---
+++ src/components/Input/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #228B22;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 28px;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #228B22;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
